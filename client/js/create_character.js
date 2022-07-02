9dcc96a38f2529b4d7de334cd87e257a4f955693
Dante Benicio <dantecosta79@gmail.com>
2022-07-02 19:04:28 -0300

    add features

--- client/js/create_character.js
+++ client/js/create_character.js
@@ -1,25 +1,57 @@
 const form = document.forms.item('char_registration')
+const removeImage = document.getElementById('remove_image')
+const { content_input, file_input } = form
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
+removeImage.addEventListener('click', () => {
+  file_input.files = null
+  const labelInputFile = file_input.previousElementSibling
+  labelInputFile.textContent = 'Add your character Image'
+})
 
-  form.previousElementSibling
-
-  const { content_input, file_input } = form
-
-  if (file_input.files) {
+file_input.addEventListener('change', function () {
+  if (this.files) {
     const labelInputFile = file_input.previousElementSibling
     const fileName = file_input.files[0].name
 
     labelInputFile.textContent = fileName
+    removeImage.style.display = 'block'
+  } else {
+    console.log('mudou')
+  }
+})
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  if (!file_input.files[0]) {
+    alert('Add an image file!')
+    return ''
   }
 
   const reader = new FileReader()
 
   reader.readAsDataURL(file_input.files[0])
 
-  reader.onload = (file) => {
-    const { currentTarget: { result } } = file
-    console.log(result)
+  reader.onload = async (file) => {
+    const { currentTarget: { result: imageBase64 } } = file
+
+    try {
+      const { status } = await fetch('http://localhost:3001/store', {
+        headers: { 'Content-Type': 'application/json'},
+        method: 'POST',
+        body: JSON.stringify({
+          img: imageBase64,
+          content: content_input.value
+        })
+      })
+
+      if (status === 201) {
+        console.log('foi')
+      }
+
+      console.log(status)
+    } catch (error) {
+      alert(error.message)
+    }
   }
 })
