0dd62fb99ed1b65b7d36b7a1c6621fc7140456a9
Dante Benicio <dantecosta79@gmail.com>
2022-07-02 19:14:47 -0300

    script update

--- client/js/script.js
+++ client/js/script.js
@@ -1,33 +1,46 @@
-import { data } from './data.js';
-
 const resultBox = document.querySelector('div.__result-box');
 const inputSearch = document.querySelector('input.input-search');
 
-// data.forEach((v, i) => {
-//     let html = `
-//     <div class="__result-content">
-//     <img src="${v.img}" alt="" class="__result-img">
-//     <span class="__result-span">${v.content}</span>
-//     </div>`
+window.onload = async () => {
+  try {
+    const response = await fetch('http://localhost:3001/get');
+    const data = await response.json();
 
-//     resultBox.innerHTML += html
-// })
+    if (!data) {
+      return ''
+    }
+
+    data.forEach(({ content, img}) => {
+      let html = `
+      <div class="__result-content">
+      <img src="${img}" alt="" class="__result-img">
+      <span class="__result-span">${content}</span>
+      </div>`
+
+      resultBox.innerHTML += html
+  })
+  } catch (error) {
+
+  }
+}
 
 inputSearch.addEventListener('keyup', (e) => {
-    let inputValue = e.target.value.toLowerCase();
-    if (inputValue != '') {
+  const inputValue = e.target.value.toLowerCase();
 
-        let findValue = data.filter((v) => {
-            return v.content.toLowerCase().replace(/<[^>]*>?/gm, '').includes(inputValue)
-        })
-        resultBox.innerHTML = ''
-        findValue.forEach(v => {
-            let html = `
+  if (inputValue != '') {
+    const findValue = data.filter((v) => {
+      return v.content.toLowerCase().replace(/<[^>]*>?/gm, '').includes(inputValue)
+    })
+
+    resultBox.innerHTML = ''
+      findValue.forEach(v => {
+          const html = `
             <div class="__result-content">
             <img src="${v.img}" alt="" class="__result-img">
             <span class="__result-span">${v.content}</span>
             </div>`
-            resultBox.innerHTML += html;
+
+          resultBox.innerHTML += html;
         })
     } else {
         resultBox.innerHTML = ''
